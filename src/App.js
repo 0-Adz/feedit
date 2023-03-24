2fb3b1b958a4b8619837bed8880b3cc97f5f20f8
0-Adz <adz.aadarsh11@gmail.com>
2023-03-24 14:55:42 +0530

    Adding top scroll bar and hiding API

--- src/App.js
+++ src/App.js
@@ -7,22 +7,37 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
+
+  apiKey=process.env.REACT_APP_NEWS_API
+
+  state={
+    progress: 0
+  }
+
+  setProgress= (progress) => {
+    this.setState({progress: progress})
+  }
   render() {
     return (
       <div>
         <Router>
         <Navbar/>
+        <LoadingBar
+        color='#dc3545'
+        progress={this.state.progress}
         
+      />
         <Routes>
-          <Route exact path="/" element = {<News key="general" pageSize={6} country ="in" category="General"/>}/>
-          <Route exact path="/Business" element = {<News key="Business" pageSize={6} country ="in" category="Business"/>}/>
-          <Route exact path="/Entertainment" element = {<News key="Entertainment" pageSize={6} country ="in" category="Entertainment"/>}/>
-          <Route exact path="/Health" element = {<News key="Health" pageSize={6} country ="in" category="Health"/>}/>
-          <Route exact path="/Science" element = {<News key="Science" pageSize={6} country ="in" category="Science"/>}/>
-          <Route exact path="/Sports" element = {<News key="Sports" pageSize={6} country ="in" category="Sports"/>}/>
-          <Route exact path="/Technology" element = {<News key="Technology" pageSize={6} country ="in" category="Technology"/>}/>
+          <Route exact path="/" element = {<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={6} country ="in" category="General"/>}/>
+          <Route exact path="/Business" element = {<News setProgress={this.setProgress} apiKey={this.apiKey} key="Business" pageSize={6} country ="in" category="Business"/>}/>
+          <Route exact path="/Entertainment" element = {<News setProgress={this.setProgress} apiKey={this.apiKey} key="Entertainment" pageSize={6} country ="in" category="Entertainment"/>}/>
+          <Route exact path="/Health" element = {<News setProgress={this.setProgress} apiKey={this.apiKey} key="Health" pageSize={6} country ="in" category="Health"/>}/>
+          <Route exact path="/Science" element = {<News setProgress={this.setProgress} apiKey={this.apiKey} key="Science" pageSize={6} country ="in" category="Science"/>}/>
+          <Route exact path="/Sports" element = {<News setProgress={this.setProgress} apiKey={this.apiKey} key="Sports" pageSize={6} country ="in" category="Sports"/>}/>
+          <Route exact path="/Technology" element = {<News setProgress={this.setProgress} apiKey={this.apiKey} key="Technology" pageSize={6} country ="in" category="Technology"/>}/>
         </Routes>
         </Router>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,9 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-info" style={{left: '80%', zIndex:'1', color: 'white'}}>{source}</span>
+        <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right:'0'}}>
+        <span className="badge rounded-pill bg-danger" style={{left: '80%', zIndex:'1', color: 'white'}}>{source}</span>
+        </div>
           <img src={!imageUrl?"http://cdn.wionews.com/sites/default/files/2023/02/17/333056-untitled-design-2023-02-17t151731065.png":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title} </h5>
